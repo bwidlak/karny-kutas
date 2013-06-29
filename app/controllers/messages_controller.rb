6bb31d2fdbf64c5b3a89eb3d15b32bfe0a846a8e
bwidlak <bartekwidlak@gmail.com>
2013-06-29 18:35:37 +0100

    conf for route to start form messges#new change the form and some house keeping

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,28 +1,20 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
 
-  # GET /messages
-  # GET /messages.json
   def index
     @messages = Message.all
   end
 
-  # GET /messages/1
-  # GET /messages/1.json
   def show
   end
 
-  # GET /messages/new
   def new
     @message = Message.new
   end
 
-  # GET /messages/1/edit
   def edit
   end
 
-  # POST /messages
-  # POST /messages.json
   def create
     @message = Message.new(message_params)
 
@@ -37,8 +29,6 @@ class MessagesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /messages/1
-  # PATCH/PUT /messages/1.json
   def update
     respond_to do |format|
       if @message.update(message_params)
@@ -51,8 +41,6 @@ class MessagesController < ApplicationController
     end
   end
 
-  # DELETE /messages/1
-  # DELETE /messages/1.json
   def destroy
     @message.destroy
     respond_to do |format|
