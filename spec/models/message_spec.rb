a6f6d7dfd30f51f52695fd3bbf6229c616d70f2d
barto <barto@Bart.local>
2013-07-01 16:57:28 +0100

    add specs for validations

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,12 +3,27 @@ require 'factory_girl'
 
 describe Message do
 
- 	describe '#search' do
+ 	describe '#on create while you send incomplete message' do
 
-		it 'should return messages with that email' do
+		it 'Name validation should not pass' do
+			FactoryGirl.build(:message_without_name).should_not be_valid
+		end
 
+		it 'Target name validation should not pass' do
+			FactoryGirl.build(:message_without_target_name).should_not be_valid
+		end
+
+		it 'Target email validation should not pass' do
+			FactoryGirl.build(:message_without_target_email).should_not be_valid
+		end
+
+		it 'Body validation should not pass' do
+			FactoryGirl.build(:message_without_target_email).should_not be_valid
 		end
 
 	end
-  
+
+	it "should have valid factory" do
+		FactoryGirl.build(:message).should be_valid
+	end
 end
